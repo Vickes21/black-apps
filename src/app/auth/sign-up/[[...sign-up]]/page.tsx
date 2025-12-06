7258eb9cc46c8733d8c66f0039cc09328c5a77a2
Vickes <vickes.dev@gmail.com>
2025-12-06 15:40:12 -0300

    main

--- src/app/auth/sign-up/[[...sign-up]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignUp } from "@clerk/nextjs";
-
-export default function SignUpPage() {
-  return (
-    <div className="w-full min-h-screen flex items-center justify-center">
-      <SignUp />
-    </div>
-  )
-}
